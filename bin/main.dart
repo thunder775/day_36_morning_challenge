a57d9d64d2e4ce3e1f836a18e2a794211660e95b
thunder775 <rahulmasih61@gmail.com>
2019-11-22 12:11:01 +0530

    all legit test cases passing

--- bin/main.dart
+++ bin/main.dart
@@ -20,6 +20,71 @@
 // ["Mary", "mary@mail.com"]]
 /// Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
 /// ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+List<List<String>> accountsMerge(List<List<String>> accounts) {
+  List<List<String>> newList = [];
+  List<List<String>> commons = [];
+  for (int i = 0; i < accounts.length; i++) {
+    for (int j = i + 1; j < accounts.length; j++) {
+      if (haveCommonMails(accounts[i], accounts[j])) {
+        commons.add(accounts[i]);
+        commons.add(accounts[j]);
+        accounts[j] = [accounts[i][0]];
+        accounts[i] = addCommonMailsWithUser(commons);
+      }
+    }
+    if (commons.isEmpty && accounts[i].length > 1) {
+      commons.add(accounts[i]);
+    }
+    if (commons.isNotEmpty) {
+      newList.add(addCommonMailsWithUser(commons));
+    }
+    commons = [];
+  }
+  print(newList);
+  return newList;
+}
+
+bool haveCommonMails(List<String> list1, List<String> list2) {
+  for (int i = 1; i < list1.length; i++) {
+    for (int j = 1; j < list2.length; j++) {
+      if (list1[i] == list2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+List addCommonMailsWithUser(List<List<String>> list) {
+  List<String> commonMails = [];
+  list.forEach((user) {
+    user.forEach((email) {
+      if (user.indexOf(email) != 0 && !(commonMails.contains(email))) {
+        commonMails.add(email);
+      }
+    });
+  });
+  commonMails.sort();
+  commonMails.insert(0, list[0][0]);
+  return commonMails;
+}
 
 main() {
+  accountsMerge([
+    ["John", "johnsmith@mail.com", "john00@mail.com"],
+    ["John", "johnnybravo@mail.com"],
+    ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+    ["Mary", "mary@mail.com"]
+  ]);
+  accountsMerge([
+    ['john', 'a', 'b'],
+    ['john', 'a', 'b'],
+    ['john', 'a', 'b'],
+    ['john', 'b', 'c'],
+    ['john', 'c', 'd'],
+    ['john', 'e', 'd'],
+    ['john', 'e', 'f'],
+    ["Mary", "mary@mail.com"]
+
+  ]);
 }
--- test/Day_36_Morning_Challenge_test.dart
+++ test/Day_36_Morning_Challenge_test.dart
@@ -1,6 +1,29 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-  test('Test Case', () {
+  test('Test Case', () {});
+  test('accounts merge', () {
+    expect(accountsMerge([
+      ["John", "johnsmith@mail.com", "john00@mail.com"],
+      ["John", "johnnybravo@mail.com"],
+      ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+      ["Mary", "mary@mail.com"]
+    ]),  [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
+    ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]);
+
+    expect( accountsMerge([
+      ['john', 'a', 'b'],
+      ['john', 'a', 'b'],
+      ['john', 'a', 'b'],
+      ['john', 'b', 'c'],
+      ['john', 'c', 'd'],
+      ['john', 'e', 'd'],
+      ['john', 'e', 'f'],
+      ["Mary", "mary@mail.com"]
+
+    ]), [['john', 'a', 'b','c','d', 'e', 'f'], ['Mary', 'mary@mail.com']]);
+
+
   });
 }
